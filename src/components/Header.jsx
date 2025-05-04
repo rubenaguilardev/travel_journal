4b41bf1b4cd00672a98505170e8f66f132a880f8
rubenaguilardev <ragui56@wgu.edu>
2025-05-04 16:31:44 -0700

    create header component and style it

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src='/src/assets/Globe.png' alt='globe icon'/>
+            <span>my travel journal.</span>
+        </header>
+    )
+}
