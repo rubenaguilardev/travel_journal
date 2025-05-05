06bb938caa4e6d0a9e4527d74ed05a2288262f41
rubenaguilardev <ragui56@wgu.edu>
2025-05-05 04:04:54 -0700

    add props

--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -1,24 +1,20 @@
-export default function Entry() {
+export default function Entry(props) {
     return (
         <section>
             <div className='image'>
-                <img src='https://scrimba.com/links/travel-journal-japan-image-url' alt='Japan moutain image'/>
+                <img src={props.img.src} alt='Japan moutain image'/>
             </div>
             <div className='entry-right'>
                 <div className='top'>
-                    <img src='/src/assets/fill.png' alt='location marker icon'/>
-                    <span className='country'>JAPAN</span>
+                    <img src='/src/assets/fill.png' alt={props.img.alt}/>
+                    <span className='country'>{props.country}</span>
                     
-                    <a className='google-link' href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu'>
+                    <a className='google-link' href={props.googleMapsLink}>
                     View on Google Maps</a>
                 </div>
-                <h2>Mount Fuji</h2>
-                <span className='dates'>12 Jan, 2023 - 24 Jan, 2023</span>
-                <p>
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters 
-                    (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, 
-                    for both Japanese and foreign tourists.
-                </p>
+                <h2>{props.title}</h2>
+                <span className='dates'>{props.dates}</span>
+                <p>{props.text}</p>
             </div>
         </section>
     )
