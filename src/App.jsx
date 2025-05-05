a71fa9685a7b55313ff814369aae3788b62f4b64
rubenaguilardev <ragui56@wgu.edu>
2025-05-05 15:55:43 -0700

    clean up attributes

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,21 @@ import Header from './components/Header'
 import Entry from './components/Entries'
 import travelData from '../data.js'
 
-console.log(travelData)
-
 export default function App() {
 
   const travelProps = travelData.map(obj => {
     return (
       <Entry
-        img={obj.img.src}
-        alt={obj.img.alt}
-        title={obj.title}
-        country={obj.country}
-        googleMapsLink={obj.googleMapsLink}
-        dates={obj.dates}
-        text={obj.text}
+        key={obj.id}
+        obj={obj}
       />
     )
   })
-  console.log(travelProps[0])
+
   return (
     <>
       <Header/>
-      {travelProps}
-      
+      {travelProps} 
     </>
   )
 }
--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -2,19 +2,19 @@ export default function Entry(props) {
     return (
         <section>
             <div className='image'>
-                <img src={props.img} alt={props.alt}/>
+                <img src={props.obj.img.src} alt={props.obj.img.imgAlt}/>
             </div>
             <div className='entry-right'>
                 <div className='top'>
                     <img src='/src/assets/fill.png' alt='location icon'/>
-                    <span className='country'>{props.country}</span>
+                    <span className='country'>{props.obj.country}</span>
                     
-                    <a className='google-link' href={props.googleMapsLink}>
+                    <a className='google-link' href={props.obj.googleMapsLink}>
                     View on Google Maps</a>
                 </div>
                 <h2>{props.title}</h2>
-                <span className='dates'>{props.dates}</span>
-                <p>{props.text}</p>
+                <span className='dates'>{props.obj.dates}</span>
+                <p>{props.obj.text}</p>
             </div>
         </section>
     )
