6827011fca65a3a340b471b72750f180a3ff3dfc
rubenaguilardev <ragui56@wgu.edu>
2025-05-05 16:11:38 -0700

    use spread operator to create attributes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     return (
       <Entry
         key={obj.id}
-        obj={obj}
+        {...obj}
       />
     )
   })
--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -1,20 +1,21 @@
 export default function Entry(props) {
+    console.log(props)
     return (
         <section>
             <div className='image'>
-                <img src={props.obj.img.src} alt={props.obj.img.imgAlt}/>
+                <img src={props.img.src} alt={props.img.alt}/>
             </div>
             <div className='entry-right'>
                 <div className='top'>
                     <img src='/src/assets/fill.png' alt='location icon'/>
-                    <span className='country'>{props.obj.country}</span>
+                    <span className='country'>{props.country}</span>
                     
-                    <a className='google-link' href={props.obj.googleMapsLink}>
+                    <a className='google-link' href={props.googleMapsLink}>
                     View on Google Maps</a>
                 </div>
                 <h2>{props.title}</h2>
-                <span className='dates'>{props.obj.dates}</span>
-                <p>{props.obj.text}</p>
+                <span className='dates'>{props.dates}</span>
+                <p>{props.text}</p>
             </div>
         </section>
     )
